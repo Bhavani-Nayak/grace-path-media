b72c77bb62c2db8eceaea84bdd07a5d371bc3cdd
Bhavani <bhawannayak02@gmail.com>
2026-07-29 14:31:04 +0530

    fixes revert to static site

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,8 +1,7 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-const config = defineCloudflareConfig({
-  buildCommand: "npm run build:next",
-});
+const config = defineCloudflareConfig({});
+config.buildCommand = "npm run build:next";
 config.default.minify = true;
 
 export default config;
